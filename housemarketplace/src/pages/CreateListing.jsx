408c261de978337a7e4dd4bbdce348732998af74
aditya gupta <guptaaditya854@gmail.com>
2022-12-29 00:33:23 +0530

    .

--- housemarketplace/src/pages/CreateListing.jsx
+++ housemarketplace/src/pages/CreateListing.jsx
@@ -95,94 +95,105 @@ function CreateListing()
     
         if (geolocationEnabled) {
           const response = await fetch(
-            `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
-          )
-    
-          const data = await response.json()
-    
-          geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
-          geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
-    
-          location =
-            data.status === 'ZERO_RESULTS'
-              ? undefined
-              : data.results[0]?.formatted_address
+            `https://us1.locationiq.com/v1/search?key=pk.c16b83653b5b349084af4f838c0e91b1&q=${address}&format=json`
+          );
+          const data = await response.json();
+          
+        if(response.status==200)
+        {
+          geolocation.lat = data[0].lat ?? 0
+              geolocation.lng = data[0].lon ?? 0
+
+              console.log(geolocation.lat)
+              console.log(geolocation.lng)
+              console.log(response.status)
+              location = data[0].display_name
+              console.log(location)
+        }
+         else{
+          location = undefined
+         }
+         
     
           if (location === undefined || location.includes('undefined')) {
             setLoading(false)
             toast.error('Please enter a correct address')
             return
           }
+
+          console.log(data)
         } else {
           geolocation.lat = latitude
           geolocation.lng = longitude
+          location=address
         }
-    
+
+     setLoading(false)
         // Store image in firebase
-        const storeImage = async (image) => {
-          return new Promise((resolve, reject) => {
-            const storage = getStorage()
-            const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+        // const storeImage = async (image) => {
+        //   return new Promise((resolve, reject) => {
+        //     const storage = getStorage()
+        //     const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
     
-            const storageRef = ref(storage, 'images/' + fileName)
+        //     const storageRef = ref(storage, 'images/' + fileName)
     
-            const uploadTask = uploadBytesResumable(storageRef, image)
+        //     const uploadTask = uploadBytesResumable(storageRef, image)
     
-            uploadTask.on(
-              'state_changed',
-              (snapshot) => {
-                const progress =
-                  (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-                console.log('Upload is ' + progress + '% done')
-                switch (snapshot.state) {
-                  case 'paused':
-                    console.log('Upload is paused')
-                    break
-                  case 'running':
-                    console.log('Upload is running')
-                    break
-                  default:
-                    break
-                }
-              },
-              (error) => {
-                reject(error)
-              },
-              () => {
-                // Handle successful uploads on complete
-                // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                  resolve(downloadURL)
-                })
-              }
-            )
-          })
-        }
+        //     uploadTask.on(
+        //       'state_changed',
+        //       (snapshot) => {
+        //         const progress =
+        //           (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        //         console.log('Upload is ' + progress + '% done')
+        //         switch (snapshot.state) {
+        //           case 'paused':
+        //             console.log('Upload is paused')
+        //             break
+        //           case 'running':
+        //             console.log('Upload is running')
+        //             break
+        //           default:
+        //             break
+        //         }
+        //       },
+        //       (error) => {
+        //         reject(error)
+        //       },
+        //       () => {
+        //         // Handle successful uploads on complete
+        //         // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        //         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        //           resolve(downloadURL)
+        //         })
+        //       }
+        //     )
+        //   })
+        // }
     
-        const imgUrls = await Promise.all(
-          [...images].map((image) => storeImage(image))
-        ).catch(() => {
-          setLoading(false)
-          toast.error('Images not uploaded')
-          return
-        })
+        // const imgUrls = await Promise.all(
+        //   [...images].map((image) => storeImage(image))
+        // ).catch(() => {
+        //   setLoading(false)
+        //   toast.error('Images not uploaded')
+        //   return
+        // })
     
-        const formDataCopy = {
-          ...formData,
-          imgUrls,
-          geolocation,
-          timestamp: serverTimestamp(),
-        }
+        // const formDataCopy = {
+        //   ...formData,
+        //   imgUrls,
+        //   geolocation,
+        //   timestamp: serverTimestamp(),
+        // }
     
-        formDataCopy.location = address
-        delete formDataCopy.images
-        delete formDataCopy.address
-        !formDataCopy.offer && delete formDataCopy.discountedPrice
+        // formDataCopy.location = address
+        // delete formDataCopy.images
+        // delete formDataCopy.address
+        // !formDataCopy.offer && delete formDataCopy.discountedPrice
     
-        const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
-        setLoading(false)
-        toast.success('Listing saved')
-        navigate(`/category/${formDataCopy.type}/${docRef.id}`)
+        // const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
+        // setLoading(false)
+        // toast.success('Listing saved')
+        // navigate(`/category/${formDataCopy.type}/${docRef.id}`)
       }
 
       const onMutate = (e) => {
